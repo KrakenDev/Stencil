f0d03c0b9b1ae11ac66131aa1dda11c9982e807d
Hector Matos <hectormatos2011@gmail.com>
2019-07-09 20:57:50 -0700

    adds swiftlint komondor capabilities

--- Package.swift
+++ Package.swift
@@ -2,21 +2,37 @@
 import PackageDescription
 
 let package = Package(
-  name: "Stencil",
-  products: [
-    .library(name: "Stencil", targets: ["Stencil"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
-    .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0")
-  ],
-  targets: [
-    .target(name: "Stencil", dependencies: [
-      "PathKit"
-    ], path: "Sources"),
-    .testTarget(name: "StencilTests", dependencies: [
-      "Stencil",
-      "Spectre"
-    ])
-  ]
+    name: "Stencil",
+    products: [
+        .library(name: "Stencil", targets: ["Stencil"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.0")
+    ],
+    targets: [
+        .target(name: "Stencil", dependencies: [
+            "PathKit"
+        ], path: "Sources"),
+        .testTarget(name: "StencilTests", dependencies: [
+            "Stencil",
+            "Spectre"
+        ])
+    ]
 )
+
+#if canImport(PackageConfig)
+import PackageConfig
+
+let config = PackageConfig([
+    "komondor": [
+        "pre-push": "swift test",
+        "pre-commit": [
+            "swift test",
+            "swift run swiftlint autocorrect --path Sources/",
+            "git add .",
+        ],
+    ],
+]).write()
+#endif
+
